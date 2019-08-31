cb7ac42b62dfb5a06b43594558d0f66f7eb3ba16
Abetang Joseph <josephabetang@gmail.com>
2019-08-31 21:27:04 +0100

    initial commit/setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mock Premier League API
+An API that serves the latest scores of fixtures of matches in a Mock Premier League
---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const router = Router();
+
+/* GET home page. */
+router.get('/', function(_req, res) {
+  res.status(200).json({ message: 'All is well, home for index' });
+});
+
+export default router;
