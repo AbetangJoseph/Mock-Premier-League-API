2ea60c318ee8c3f16fec641d49709bae60ae5aa2
Abetang Joseph <josephabetang@gmail.com>
2019-09-02 02:31:52 +0100

    mount user route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import userRouter from './user';
 
 const router = Router();
 
 /** GET /health-check - Check service health */
 router.get('/health-check', (_req, res) => res.send('OK'));
+
+// mount user route
+router.use('/user', userRouter);
 
 export default router;
