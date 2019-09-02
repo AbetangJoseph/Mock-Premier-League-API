f90ed46f8c1cf44693535d67c7cf3654ac7dc968
Abetang Joseph <josephabetang@gmail.com>
2019-09-02 02:27:13 +0100

    setup test DB

---
+++ testConfig/db.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+export const DBconnect = async () => {
+  await mongoose
+    .connect('mongodb://localhost:27017/premierAPI', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log('connection successful'))
+    .catch(err => {
+      console.error(err.message);
+      process.exit(1);
+    });
+};
+
+export async function DBdisconnect() {
+  await mongoose.connection.db.dropDatabase();
+  mongoose.connection.close();
+  console.log('connection closed');
+}
