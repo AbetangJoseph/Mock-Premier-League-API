b0d8e41290236a18c5fcd920c3d421b744586f8e
Abetang Joseph <josephabetang@gmail.com>
2019-09-02 15:50:28 +0100

    add tests for login route

--- __tests__/routes/user.test.ts
+++ __tests__/routes/user.test.ts
@@ -63,4 +63,59 @@ describe('TESTS FOR USER ROUTE', () => {
         });
     });
   });
+
+  describe('login route', () => {
+    const userWithAMissingCredential = {
+      email: 'joeabetang@gmail.com',
+      password: '',
+    };
+
+    const userWithRightCredentials = {
+      email: 'joeabetang@gmail.com',
+      password: 'pass1234',
+    };
+
+    const userWithWrongCredentials = {
+      email: 'joeabetang@gmail.com',
+      password: 'pass123',
+    };
+
+    it('validates and send an appropraite status code and message if a field is missing', async () => {
+      await request(app)
+        .post('/api/v1/user/login')
+        .send(userWithAMissingCredential)
+        .set('Accept', 'application/json')
+        .then(res => {
+          expect(res.status).toBe(400);
+          expect(res.body.error).toMatch('password is not allowed to be empty');
+        });
+    });
+
+    it('sends appropriate status code and throws error if credentials are invalid', async () => {
+      await request(app)
+        .post('/api/v1/user/login')
+        .send(userWithWrongCredentials)
+        .set('Accept', 'application/json')
+        .then(res => {
+          expect(res.status).toBe(400);
+          expect(res.body.error).toMatch('Invalid email or password');
+        });
+    });
+
+    it('returns "success":true and a jwt token if login is successful', async () => {
+      await request(app)
+        .post('/api/v1/user/login')
+        .send(userWithRightCredentials)
+        .set('Accept', 'application/json')
+        .then(res => {
+          expect(res.status).toBe(200);
+          expect(res.body).toEqual(
+            expect.objectContaining({
+              success: true,
+              token: expect.any(String),
+            }),
+          );
+        });
+    });
+  });
 });
