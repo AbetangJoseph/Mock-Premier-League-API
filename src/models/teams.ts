aec1812869c85076f0333e8642069efdbe565fdb
Abetang Joseph <josephabetang@gmail.com>
2019-09-03 15:42:25 +0100

    add Teams Schema

---
+++ src/models/teams.ts
@@ -0,0 +1,30 @@
+import { prop, Typegoose } from 'typegoose';
+
+class Teams extends Typegoose {
+  @prop({ required: true, trim: true })
+  clubName!: string;
+
+  @prop({ required: true, trim: true })
+  clubCodeName!: string;
+
+  @prop({ required: true })
+  founded!: number;
+
+  @prop({ required: true, trim: true })
+  coach!: string;
+
+  @prop({ trim: true, default: 'logo' })
+  logo?: string;
+
+  @prop({ required: true, trim: true })
+  country!: string;
+
+  @prop({ required: true, trim: true })
+  stadium!: string;
+
+  @prop({ required: true })
+  stadiumCapacity!: number;
+}
+
+const TeamModel = new Teams().getModelForClass(Teams);
+export { TeamModel };
