e18b75fca8c3ea43991a7fd4bd986e23ae86db96
Abetang Joseph <josephabetang@gmail.com>
2019-09-03 15:47:57 +0100

    mount teamRouter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import userRouter from './user';
+import teamRouter from './teams';
 
 const router = Router();
 
@@ -8,5 +9,8 @@ router.get('/health-check', (_req, res) => res.send('OK'));
 
 // mount user route
 router.use('/user', userRouter);
+
+// mount team route
+router.use('/teams', teamRouter);
 
 export default router;
