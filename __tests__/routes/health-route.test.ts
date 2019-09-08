8940f7378df81727da4ce905b1e8c748bf4b0479
Abetang Joseph <josephabetang@gmail.com>
2019-09-08 01:01:00 +0100

    refactor health-check test

--- __tests__/routes/health-route.test.ts
+++ __tests__/routes/health-route.test.ts
@@ -4,7 +4,7 @@ import app from '../../src/app';
 describe('HEALTH-CHECK', () => {
   it('should check that router on routes/index is mounted on app properly', async () => {
     await request(app)
-      .get('/api/v1/health-check')
+      .get('/')
       .set('Accept', 'application/json')
       .then(res => {
         expect(res.status).toBe(200);
